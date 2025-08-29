54e12339f6f382a16cf628913fc9a40fdbc3683d
Michael Rolli <michael@rollis.ch>
2025-08-29 14:44:59 +0200

    build: add azcli-based infrastructure setup script

---
+++ infra/provision.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+workload_name="hugo-static-app"
+region=westeurope
+
+resource_group_name=$(printf "rg-%s" "$workload_name")
+static_web_app_name=$(printf "stapp-%s" "$workload_name")
+static_web_app_sku="Free"
+
+az group create \
+  --name "$resource_group_name" \
+  --location $region
+
+az staticwebapp create \
+  --name "$static_web_app_name" \
+  --resource-group "$resource_group_name" \
+  --location $region \
+  --source "$(git remote -v | awk '/push/{print $2}')" \
+  --branch "main" \
+  --sku "$static_web_app_sku" \
+  --login-with-github
+
+webapp_url=$(az staticwebapp show --name "$static_web_app_name" --resource-group "$resource_group_name" --query "defaultHostname" -o tsv)
+
+runtime="+1M"
+endtime=$(date -v${runtime} +%s)
+
+while [[ $(date +%s) -le $endtime ]]; do
+  if curl -I -s "$webapp_url" >/dev/null; then
+    curl -L -s "$webapp_url" 2>/dev/null | head -n 9
+    break
+  else
+    sleep 10
+  fi
+done
+
+echo "You can now visit your web server at https://$webapp_url"
+
+echo -e "\nTo deploy the code using the SWA CLI run the command\n  npx swa login --resource-group $resource_group_name --app-name $static_web_app_name"
